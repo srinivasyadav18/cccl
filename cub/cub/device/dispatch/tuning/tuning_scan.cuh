a09c29e2c461b9607e8835b2655680aff58a0868
Srinivas Yadav <ssinganaboin@nvidia.com>
2026-06-08 10:55:18 -0500

    nits

--- cub/cub/device/dispatch/tuning/tuning_scan.cuh
+++ cub/cub/device/dispatch/tuning/tuning_scan.cuh
@@ -1034,7 +1034,7 @@ struct policy_selector
   {
     // we first try to get the valid warpspeed implementation. if we can't run it, fall back to the old scan impl.
     // For stable reduction order (fp + plus), warpspeed can only be used on sm_100+, Older arches fall back to classic
-    // lookback stable reduction order verison below.
+    // lookback stable reduction order implementation below.
     if (!require_stable_reduction_order || cc >= ::cuda::compute_capability{10, 0})
     {
       const auto warpspeed_policy_opt = get_warpspeed_policy(cc);
